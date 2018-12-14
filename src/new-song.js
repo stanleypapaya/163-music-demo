a0efb36d7949b4bea54b01236ad3f430cb56a5eb
stanleypapaya <stanleychenxingyu@gmail.com>
2018-12-15 05:02:31 +0800

    song-list MVC

---
+++ src/new-song.js
@@ -0,0 +1,20 @@
+{
+    let view = {
+        el: '.newSong',
+        template: `
+        <h1>新建歌曲</h1>
+        `,
+        render(data){
+            $(this.el).html(this.template)
+        }
+    }
+    let model = {}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.view.render(this.model.data)
+        }
+    }
+    controller.init(view,model)
+}
