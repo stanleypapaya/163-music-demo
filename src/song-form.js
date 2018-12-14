4b567907bfc294cec2eb6a56294ca4f41074a983
stanleypapaya <stanleychenxingyu@gmail.com>
2018-12-14 23:30:56 +0800

    song-form MVC

---
+++ src/song-form.js
@@ -0,0 +1,36 @@
+{
+    let view = {
+        el: '.page > main',
+        template: `
+        <form class="detail">
+            <div class="row">
+                <label>歌名：</label>
+                <input type="text">
+            </div>
+            <div class="row">
+                <label>歌手：</label>
+                <input type="text">                   
+            </div>
+            <div class="row">
+                <label>外链：</label>
+                <input type="text">
+            </div>
+            <div class="row">
+                <button type="submit">保存</button>
+            </div>
+        </form>
+        `,
+        render(data){
+            $(this.el).html(this.template)
+        }
+    }
+    let model = {}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.view.render(this.model.data)
+        }
+    }
+    controller.init(view,model)
+}
